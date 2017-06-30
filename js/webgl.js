403e2bebd0114d7f620ad9b8a06c04dca4e4c75b
Alexandr Poltavsky <alexpolt@yandex.ru>
2017-06-30 23:43:38 +0300

    .

--- js/webgl.js
+++ js/webgl.js
@@ -388,6 +388,7 @@ function activate_webgl() {
       if( offw ) e.style.width = offw + "%";
       img.onload = function() {
         var cw = document.getElementById( "content" ).offsetWidth;
+        e.style.width = "auto";
         var offw = Math.round( 100 * img.offsetWidth / cw );
         if( offw ) e.style.width = offw + "%";
       };
